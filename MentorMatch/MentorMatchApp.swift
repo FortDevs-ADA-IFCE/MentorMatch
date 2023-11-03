0d9151849026887afbc7ca6f91ffa3bf7dcca88c
Joel Lacerda <joellacerdaol@gmail.com>
2023-11-03 08:31:35 -0300

    Initial Commit

---
+++ MentorMatch/MentorMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentorMatchApp.swift
+//  MentorMatch
+//
+//  Created by Joel Lacerda on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MentorMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
